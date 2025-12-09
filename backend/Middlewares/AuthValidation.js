1e9fd45d0176a0c476622c3b264ce7211ad50f4a
Harsh Gupta <134432719+harsh232gupta@users.noreply.github.com>
2025-12-09 21:09:55 +0530

    Refactor signupValidation error message
    Updated error message in signup validation to be more user-friendly.

--- backend/Middlewares/AuthValidation.js
+++ backend/Middlewares/AuthValidation.js
@@ -1,23 +1,6 @@
 
 const Joi = require('joi');
 
-// const signupValidation = (req, res, next) => {
-//     const schema = Joi.object({
-//         name: Joi.string().min(3).max(100).required(),
-//         email: Joi.string().email().required(),
-//         password: Joi.string().min(4).max(100).required()
-//     });
-//     const { error } = schema.validate(req.body);
-//     if (error) {
-//         return res.status(400)
-//             .json({ message: "Bad request", error })
-//     }
-//     next();
-// }
-
-
-
-
 const signupValidation = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(3).max(100).required(),
@@ -31,7 +14,7 @@ const signupValidation = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
         return res.status(400).json({ 
-            message: "Bad request", 
+            message: "Bad request hai bhai", 
             error: error.details.map(err => err.message) // More readable error messages
         });
     }
